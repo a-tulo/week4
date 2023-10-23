b980f1a5170c511d089d0c83ce426ccd0b4c5da8
alexandre.tulodziecki <alexandre.tulodziecki@yunextraffic.com>
2023-10-23 13:12:41 +0100

    Added code to demonstrate the use of built-in functions; len, ord

---
+++ built_in_functions.py
@@ -0,0 +1,7 @@
+print("Program Started!")
+user_char = input("Please enter a standard character\n")
+
+if len(user_char) == 1:
+    print(f"The ASCII code for {user_char} is {ord(user_char)}")
+
+print("Program Ended!")
