1646e3d266cf5e740b4d09a027fdd3fc9a39d7d7
alexandre.tulodziecki <alexandre.tulodziecki@yunextraffic.com>
2023-10-23 13:20:29 +0100

    Added code to demonstrate the use of built-in functions: abs, range, chr

--- built_in_functions.py
+++ built_in_functions.py
@@ -1,7 +1,19 @@
-print("Program Started!")
-user_char = input("Please enter a standard character\n")
+# print("Program Started!")
+# user_char = input("Please enter a standard character\n")
+#
+# if len(user_char) == 1:
+#     print(f"The ASCII code for {user_char} is {ord(user_char)}")
+#
+# print("Program Ended!")
 
-if len(user_char) == 1:
-    print(f"The ASCII code for {user_char} is {ord(user_char)}")
+print("Program Started!")
+
+print("Please enter an ASCII code:")
+code = int(input())
+
+if code >= 32 and code <= 126:
+    print(f"The character represented by the ASCII value {code} is: {chr(code)}")
+else:
+    print("The character cannot be displayed.")
 
 print("Program Ended!")
