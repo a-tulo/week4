014c18dcf432c5b9e0bf442798c5e394ec5ac2f0
alexandre.tulodziecki <alexandre.tulodziecki@yunextraffic.com>
2023-10-23 13:29:52 +0100

    Added code to demonstrate the use of a user-defined function with a parameter

--- custom_functions.py
+++ custom_functions.py
@@ -2,4 +2,23 @@ def listen():
     user_sound = input("Enter a word representing a sound\n")
     print(f"That was a loud {user_sound}!")
 
-listen()
+def identify():
+    user_vision = input("What do you see?\n")
+    if user_vision == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine.")
+
+def escape_by(plan):
+    if plan == "jumping over":
+        print("We cannot escape that wsy! The boulder is too big!")
+    elif plan == "running around":
+        print("We cannot escape that way! The boulder is moving too fast")
+    elif plan == "cross bridge ahead":
+        print("That might just work! Let's go!")
+    else:
+        print("We cannot escape that way! The boulder is in the way!")
+
+escape_by("jumping over")
+escape_by("running around")
+escape_by("cross bridge ahead")
